8481e06a3ebb54e2d96ee5d6c5caba7b8c104410
Adilmojangos8246 <67742933+Adilmojangos8246@users.noreply.github.com>
2020-12-12 09:13:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 function preload()
+
 {
 	
 }
@@ -17,8 +18,19 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-
-
+  	tree = new Tree(170,600,200,250)
+ 	 stone = new Stone(100,500,50,50);
+ 	 ground = new Ground(400,675,800,50);
+ 	 sling = new SlingShot(stone.body,{x:104,y:533});
+ 	 m1 = new mango(700,375,50,50);
+	 m2 = new mango(600,300,50,50);
+	 m3 = new mango(650,340,50,50);
+	 m4 = new mango(560,300,50,50);
+	 m5 = new mango(470,340,50,50);
+	 m6 = new mango(760,340,50,50);
+	 m7 = new mango(780,390,50,50);
+	 boy = new boy(170,600,200,250);
+ 
 	Engine.run(engine);
   
 }
@@ -27,10 +39,50 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
+  stone.display();
+  ground.display();
+  sling.display();
+  tree.display();
+  m1.display();
+  m2.display();
+  m3.display();
+  m4.display();
+  m5.display();
+  m6.display();
+  m7.display();
+  boy.display();
+
+  detectollision(stone,m1)
+  detectollision(stone,m2)	
+  detectollision(stone,m3)
+  detectollision(stone,m4)
+  detectollision(stone,m5)
+  detectollision(stone,m6)
+  detectollision(stone,m7)
+
   
+
   drawSprites();
  
 }
 
+function detectollision(lstone,lmango){
+	mangoBodyPosition = lmango.body.positon
+	stoneBodyPosition = lstone.body.positon
+
+	var distance = dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y)
+		 if(distance <= lmango.r + lstone.r){
+			 Matter.body.setStatic(lmango,body,false)
+		 }
+}
+
+function keypressed(){
+    
+    if (keyCode === 32 ){
+      
+        Matter.Body.setPosition(Stone.body, {x:235,y :420})
+        sling.attach(Stone.body);
+    }   
+}  
 
 
